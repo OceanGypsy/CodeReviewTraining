332e710ac5407fe1d574637df2035ffdd4870ed8
Oleksandra Revizina <orev@softserveinc.com>
2015-05-20 12:52:15 +0300

    Initial Commit

---
+++ CodeReview/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CodeReview
+//
+//  Created by Oleksandra Revizina on 5/20/15.
+//  Copyright (c) 2015 Softserve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CodeReview/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CodeReview
+//
+//  Created by Oleksandra Revizina on 5/20/15.
+//  Copyright (c) 2015 Softserve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
